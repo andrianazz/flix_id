0af79df671bd312acb85f78be366ef401628bcb3
andrianazz <andrianwahyu41@gmail.com>
2023-09-28 12:35:05 +0700

    feat: user data provider

--- lib/presentation/providers/user_data/user_data_provider.dart
+++ lib/presentation/providers/user_data/user_data_provider.dart
@@ -1,16 +1,23 @@
+import 'dart:io';
+
 import 'package:flix_id/domain/entities/result.dart';
 import 'package:flix_id/domain/entities/user.dart';
 import 'package:flix_id/domain/usecases/get_logged_in_user/get_logged_in_user.dart';
 import 'package:flix_id/domain/usecases/login/login.dart';
-import 'package:flix_id/domain/usecases/logout/logout.dart';
 import 'package:flix_id/domain/usecases/register/register.dart';
 import 'package:flix_id/domain/usecases/register/register_params.dart';
+import 'package:flix_id/domain/usecases/top_up/top_up.dart';
+import 'package:flix_id/domain/usecases/top_up/top_up_params.dart';
+import 'package:flix_id/domain/usecases/upload_profile_picture/upload_profile_picture.dart';
+import 'package:flix_id/domain/usecases/upload_profile_picture/upload_profile_picture_params.dart';
 import 'package:flix_id/presentation/providers/movie/now_playing_provider.dart';
 import 'package:flix_id/presentation/providers/movie/upcoming_provider.dart';
 import 'package:flix_id/presentation/providers/usecases/get_logged_in_user_provider.dart';
 import 'package:flix_id/presentation/providers/usecases/login_provider.dart';
 import 'package:flix_id/presentation/providers/usecases/logout_provider.dart';
 import 'package:flix_id/presentation/providers/usecases/register_provider.dart';
+import 'package:flix_id/presentation/providers/usecases/top_up_provider.dart';
+import 'package:flix_id/presentation/providers/usecases/upload_profile_picture_provider.dart';
 import 'package:flutter/foundation.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -87,12 +94,39 @@ class UserData extends _$UserData {
     var logout = ref.read(logoutProvider);
     var result = await logout(null);
 
-    switch (result) {
-      case Success(value: _):
-        state = const AsyncData(null);
-      case Failed(:final message):
-        state = AsyncError(FlutterError(message), StackTrace.current);
-        state = AsyncData(state.valueOrNull);
+    if (result case Success(value: _)) {
+      state = const AsyncData(null);
+    } else {
+      state = AsyncError(FlutterError("Logout failed"), StackTrace.current);
+      state = AsyncData(state.valueOrNull);
+    }
+  }
+
+  Future<void> topUp({required int amount}) async {
+    TopUp topUp = ref.read(topUpProvider);
+
+    String? userId = state.valueOrNull!.uid;
+
+    if (userId == null) {
+      var result = await topUp(TopUpParams(userId: userId, amount: amount));
+
+      if (result.isSuccess) {
+        refreshUserData();
+        //TODO: Refresh transaction
+      }
+    }
+  }
+
+  Future<void> uploadUserData(
+      {required User user, required File imageFile}) async {
+    UploadProfilePicture uploadProfilePicture =
+        ref.read(uploadProfilePictureProvider);
+
+    var result = await uploadProfilePicture(
+        UploadProfilePictureParams(user: user, filePath: imageFile));
+
+    if (result case Success(value: final user)) {
+      state = AsyncData(user);
     }
   }
 
