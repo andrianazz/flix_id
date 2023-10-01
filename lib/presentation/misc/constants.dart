a3805444b996de53f2127a31dd3be0d04d89ef80
andrianazz <andrianwahyu41@gmail.com>
2023-10-01 15:34:54 +0700

    fix: change backgroundColor

--- lib/presentation/misc/constants.dart
+++ lib/presentation/misc/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const Color backgroundColor = Color(0x0ff1fd2b);
+const Color backgroundColor = Color(0xff1f1d2b);
 const Color ghostWhite = Color(0xFFF6FBFF);
 const Color saffron = Color(0xFFF4C950);
